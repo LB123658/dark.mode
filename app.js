2595470ce7533ea6a8f34eff2a97391c00ab3211
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-04 16:50:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ mode.style.position = "absolute";
 mode.style.top = "0px";
 mode.style.left = "0px";
 mode.style.width = "100%";
-mode.style.height = "300%";
+mode.style.height = document.body.scrollHeight;
 mode.style.background = "#282828";
 mode.style.zIndex = "-1";
 document.body.appendChild(mode);
