63ac74737bb7fe3d0bb200ba0cdbbb65925243a7
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-04 17:10:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // create dark background
 var mode = document.createElement("DIV");
-mode.style.position = "absolute";
+mode.style.position = "fixed";
 mode.style.top = "0px";
 mode.style.left = "0px";
 mode.style.width = "100%";
-mode.style.height = "100%;
+mode.style.height = "100%";
 mode.style.background = "#282828";
 mode.style.zIndex = "-1";
 document.body.appendChild(mode);
