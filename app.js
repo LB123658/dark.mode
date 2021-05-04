85e7b3a8552e6011e7a619dcd95f9e3919d25ded
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-04 15:55:03 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var mode = document.createElement("DIV");
+mode.style.position = "absolute";
+mode.style.top = "0px";
+mode.style.left = "0px";
+mode.style.width = "100%";
+mode.style.height = "100%";
+mode.style.background = "#282828";
+mode.style.zIndex = "-1";
+document.body.appendChild(mode);
