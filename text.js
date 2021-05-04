2a66fb3db46cd97e5d0f3e0aa563caab9c5a61c5
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-04 17:10:51 -0500

    Update text.js

--- text.js
+++ text.js
@@ -4,3 +4,13 @@ var i;
 for (i = 0; i < x.length; i++) {
   x[i].style.color = "grey";
 }
+// create dark background
+var mode = document.createElement("DIV");
+mode.style.position = "fixed";
+mode.style.top = "0px";
+mode.style.left = "0px";
+mode.style.width = "100%";
+mode.style.height = "100%";
+mode.style.background = "#282828";
+mode.style.zIndex = "-1";
+document.body.appendChild(mode);
