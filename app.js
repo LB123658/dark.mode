939eb30f8b5d47b42d992ef7ec523d400b303aaa
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-04 17:05:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,4 @@ mode.style.height = "100%;
 mode.style.background = "#282828";
 mode.style.zIndex = "-1";
 document.body.appendChild(mode);
-// change text to grey so it is readable
-var x = document.getElementsByTagName("P");
-var i;
-for (i = 0; i < x.length; i++) {
-  x[i].style.color = "grey";
-}
+
