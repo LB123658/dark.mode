a3cee8c5080b29e5191204db50a9b31eab573fbf
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-04 17:52:53 -0500

    Update text.js

--- text.js
+++ text.js
@@ -4,6 +4,12 @@ var i;
 for (i = 0; i < x.length; i++) {
   x[i].style.color = "grey";
 }
+// change links to grey so it is readable
+var x = document.getElementsByTagName("A");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.color = "grey";
+}
 // create dark background
 var mode = document.createElement("DIV");
 mode.style.position = "fixed";
