97e5ea85bc6d58f8989f06474ba0a1c655049de9
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-05 10:00:36 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
 function on() {
-window.location.search == "?mode=dark";
+window.location.search = "?mode=dark";
 }
 function off() {
-window.location.search == "?mode=light";
+window.location.search = "?mode=light";
 }
-if (location.search.split("&")[0] == "?mode=dark") {
+if (window.location.search == "?mode=dark") {
 var script = document.createElement("SCRIPT");
 script.src = "https://lb123658.github.io/dark.mode/text.js";
 document.body.appendChild(script);
