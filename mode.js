cb2d3f13145df2de8ad72c14a399856a8e19f60e
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-05 13:01:02 -0500

    Update mode.js

--- mode.js
+++ mode.js
@@ -18,7 +18,28 @@ for (i = 0; i < x.length; i++) {
   x[i].style.color = "grey";
   x[i].style.background = "#282828";
 }
-
+// change buttons to grey so it looks better
+var x = document.getElementsByTagName("BUTTON");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.color = "grey";
+  x[i].style.background = "#282828";
+  x[i].style.border = "1px solid grey";
+}
+// change divs to grey so it looks better
+var x = document.getElementsByTagName("DIV");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.background = "#282828";
+}
+// change textarea to black so they look better
+var x = document.getElementsByTagName("TEXTAREA");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.color = "lightgrey";
+  x[i].style.background = "black";
+}
+//changes background with js in case <body> is already a different color
 var script = document.createElement("SCRIPT");
 script.src = "https://lb123658.github.io/dark.mode/text.js";
 document.body.appendChild(script);
