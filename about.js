7bfb04c776f916b49ff55febd86b7b4d07667efa
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-04 19:27:12 -0500

    Create about.js

---
+++ about.js
@@ -0,0 +1,8 @@
+var site = document.createElement("EMBED");
+site.style.position = "absolute";
+site.style.top = "0px";
+site.style.left = "0px";
+site.style.width = "100%";
+site.style.height = "100%";
+site.src = "https://lb123658.github.io/dark.mode/";
+document.body.appendChild(site);
