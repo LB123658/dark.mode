0aff8ab49cbc1bb2d8039324ea3d104dcf5ed87c
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-05 13:33:14 -0500

    Update and rename 404.js to buttons.js

--- 404.js
+++
@@ -1,4 +0,0 @@
-Error: this page does not exist
-Try https://lb123658.github.io/dark.mode/ for more information
-*****************************************************************
-  
---
+++ buttons.js
@@ -0,0 +1,28 @@
+// THIS ONLY WORKS WITH THIS SCRIPT ALREADY IN THE HTML: <script src="https://lb123658.github.io/dark.mode/functions.js"></script>
+//creates google icons css
+var l = document.createElement("LINK");
+l.rel = "stylesheet";
+l.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+document.head.appendChild(l);
+
+// creates button to turn on light mode
+var light = document.createElement("SPAN");
+light.className = "material-icons";
+light.id = "light-mode-button";
+light.style.background = "transparent";
+light.style.border = "none";
+light.style.color = "grey";
+light.addEventListener("click", off());
+document.body.appendChild(light);
+
+// creates button to turn on dark mode
+var dark = document.createElement("SPAN");
+dark.className = "material-icons";
+dark.id = "dark-mode-button";
+dark.style.background = "transparent";
+dark.style.border = "none";
+dark.style.color = "grey";
+dark.addEventListener("click", on());
+document.body.appendChild(dark);
+
+// to style these buttons the id for the one that turns on light mode is "light-mode-button" and the id for the one that turns dark mode on is "dark-mode-button"
