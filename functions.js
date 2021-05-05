cdf1fc94fc8ff86ddd0ffa0ebfa135df1eeb108f
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-05 08:08:29 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
 function on() {
-var script = document.createElement("SCRIPT");
-script.src = "https://lb123658.github.io/dark.mode/text.js";
-script.id = "dark-mode-script";
-document.body.appendChild(script);
+window.location.search == "?mode=dark";
 }
 function off() {
-var darkMode = document.getElementById("dark-mode-script");
-darkMode.remove();
+window.location.search == "?mode=light";
+}
+if (location.search.split("&")[0] == "?mode=dark") {
+var script = document.createElement("SCRIPT");
+script.src = "https://lb123658.github.io/dark.mode/text.js";
+document.body.appendChild(script);
+} else {
+  ;
 }
